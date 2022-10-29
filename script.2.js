6ab8ad114816a561949ee6cf2429f666a082fcf6
Elmar Quluzade <elmarguluzade1@gmail.com>
2022-10-29 17:55:28 +0400

    update.1

--- script.2.js
+++ script.2.js
@@ -1,5 +1,3 @@
-
-
 const btn_start = document.querySelector("#btnStart");
 const btn_next = document.querySelector("#nextStart");
 const question_text = document.querySelector(".question-text");
@@ -9,28 +7,26 @@ const cards_id = document.querySelector("#cards");
 
 let count = 0;
 
-
-
-function Quiz(questionText, answerOption, correctAnswer) {
-    this.questionText = questionText;
-    this.answerOption = answerOption;
-    this.correctAnswer = correctAnswer;
+function Quiz(question, answers, correctVariant) {
+    this.question = question;
+    this.answers = answers;
+    this.correctVariant = correctVariant;
 };
 
-Quiz.prototype.checkAnswer = function (answer) 
-{return answer === this.correctAnswer};
+Quiz.prototype.checkAnswer = function (correctVariant) {
+    return correctVariant == this.correctVariant;
+};
 
 btn_start.addEventListener('click', () => {
     displayQuiz();
 });
 
 btn_next.addEventListener('click', () => {
-    if (count < questions.length - 1) {
+   if (count < questions.length - 1) {
         count++;
         displayQuiz();
         btn_next.classList.remove("active");
-    } else {
-
+   } else {
         cards_id.classList.add("noshow");
         over_id.classList.remove("noshow");
         displayQuiz();
@@ -39,28 +35,24 @@ btn_next.addEventListener('click', () => {
 
 const displayQuiz = () => {
     let html = '';
-    let text = `<span><b>${questions[count].questionText}</b> sözünün ingiliscə qarşılqı nədir</span>`;
+    let text = `<span><b>${questions[count].question}</b> sözünün ingiliscə qarşılqı nədir</span>`;
 
-    for (let a in questions[count].answerOption) {
-        html += `<div onclick='chooseBtn(${count},"${a}")'; class="option"><span><b>${a}:</b> &nbsp ${questions[count].answerOption[a]}</span></div>`;
+    for (let a in questions[count].answers) {
+        html += `<div onclick='chooseVariant(${count},"${a}")'; class="option"><span><b>${a}:</b> &nbsp ${questions[count].answers[a]}</span></div>`;
      };
-
     quiz_box.innerHTML = html;
     question_text.innerHTML = text;
-
 };
-function chooseBtn(count, option) {
+
+const chooseVariant = (count, option) => {
     btn_next.classList.add("active");
 
     if (questions[count].checkAnswer(option) === true) {
         console.log("true");
-        
-
     }else {
-
         console.log("false");
     };
 
-    console.log(option);
+    console.log(count)
+    console.log(option)
 }
-
