126aa1635033fa47af26c32bb4e2959cf4ad3589
Elmar Quluzade <elmarguluzade1@gmail.com>
2022-10-31 01:12:17 +0400

    update-2

--- script.2.js
+++ script.2.js
@@ -5,7 +5,9 @@ const quiz_box = document.querySelector(".quiz-box");
 const over_id = document.querySelector("#overId");
 const cards_id = document.querySelector("#cards");
 
+
 let count = 0;
+let selectVariant = null;
 
 function Quiz(question, answers, correctVariant) {
     this.question = question;
@@ -38,21 +40,53 @@ const displayQuiz = () => {
     let text = `<span><b>${questions[count].question}</b> sözünün ingiliscə qarşılqı nədir</span>`;
 
     for (let a in questions[count].answers) {
-        html += `<div onclick='chooseVariant(${count},"${a}")'; class="option"><span><b>${a}:</b> &nbsp ${questions[count].answers[a]}</span></div>`;
+        html += `<div id="${a}" onclick='chooseVariant(this,${count},"${a}")'; class="option"><span"><b> &nbsp ${a}:</b> &nbsp ${questions[count].answers[a]}</span></div>`;
      };
     quiz_box.innerHTML = html;
     question_text.innerHTML = text;
+
 };
 
-const chooseVariant = (count, option) => {
-    btn_next.classList.add("active");
+chooseVariant = (element,count, option) => {
+         btn_next.classList.add("active");  
+         quiz_box.querySelectorAll(".option").forEach(variant => variant.classList.remove("bg-primary"))
+         element.classList.add("bg-primary");
 
-    if (questions[count].checkAnswer(option) === true) {
-        console.log("true");
-    }else {
-        console.log("false");
-    };
+         selectVariant = option;
 
-    console.log(count)
-    console.log(option)
+    //     displayQuiz();
+        // this.classList.add("bg-warning");
+      //  answer.classList.add("bg-danger");
+
+        // const opr = quiz_box.querySelectorAll(".option");
+        
+          
+          
+        // for(let a of opr){
+        //     console.log(a);
+        //     a.classList.add("bg-danger");}
+     
+        // if (questions[count].checkAnswer(option) === true) {
+        //     console.log("true");
+        //     }else {
+        //     console.log("false");
+        //     };
+
+        // console.log(option)
 }
+ 
+btn_next.addEventListener("click",(e) =>
+{
+    if(selectVariant == dogruVariant){
+        
+    }
+})
+
+ // const opr = quiz_box.querySelectorAll(".option");
+   
+    // console.log(option)
+
+    // for(let a of opr)
+    // {  
+        
+    // }
